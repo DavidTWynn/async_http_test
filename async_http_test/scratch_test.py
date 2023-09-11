a78f5a491dfdf3fdda42de720dff749cbad08037
david <davidtwynn@gmail.com>
2023-09-11 14:57:55 -0400

    Testing treasury api.

---
+++ async_http_test/scratch_test.py
@@ -0,0 +1,31 @@
+import httpx
+import rich
+
+
+def get_data():
+    base_url = (
+        "https://api.fiscaldata.treasury.gov",
+        "/services/api/fiscal_service",
+    )
+    endpoint = (
+        "/v1/accounting/od/securities_sales",
+        "?sort=-record_date&format=json",
+    )
+    url = f"{base_url}{endpoint}"
+    # parameters = {
+    #     "fields": ["country_currency_desc", "exchange_rate", "record_date"],
+    #     "filter": "record_date:gte:2015-01-01",
+    # }
+    headers = {"accept": "application/json"}
+
+    response = httpx.get(url=url, headers=headers)
+
+    rich.print(response.json())
+
+
+def main() -> None:
+    get_data()
+
+
+if __name__ == "__main__":
+    main()
